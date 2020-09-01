9b0320f5d7826237bfdc54684630cae6aed8ab0e
sinhhn <h.sinh@bravesoft.co.jp>
2020-09-01 16:50:22 +0900

    add driver

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 sudo yum install -y java
 sudo yum install -y graphviz
 curl -o schemaspy.jar https://github.com/schemaspy/schemaspy/releases/download/v6.1.0/schemaspy-6.1.0.jar
+curl https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.49.tar.gz
 java -jar $PWD/schemaspy.jar -configFile $PWD/schemaspy.properties
